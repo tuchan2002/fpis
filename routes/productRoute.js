fade1fddce06d277c41b345de8fbb45d5fb5ae20
tuchan2002 <trananhtu171002@gmail.com>
2023-07-31 22:08:21 +0700

    Push smart contract v1

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -3,5 +3,6 @@ const productController = require("../controllers/productController");
 const isAuth = require("../middlewares/auth");
 
 router.get("/", isAuth, productController.getProductsOwned);
+router.post("/", productController.createProduct); // only manufactory
 
 module.exports = router;
