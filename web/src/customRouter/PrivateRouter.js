68b37d31d399dda4a0d43a842f7e1dd2f6d4e23a
tuchan2002 <trananhtu171002@gmail.com>
2023-08-27 11:19:02 +0700

    change react to nextjs

--- web/src/customRouter/PrivateRouter.js
+++
@@ -1,8 +0,0 @@
-import { Navigate } from "react-router-dom";
-
-const PrivateRouter = ({ children }) => {
-  const loggedIn = localStorage.getItem("loggedIn");
-  return loggedIn ? children : <Navigate to="/" />;
-};
-
-export default PrivateRouter;
--- web/src/utils/constants.js
+++
@@ -1,7 +0,0 @@
-export const roles = [
-  { roleValue: 0, text: "Manufactory" },
-  { roleValue: 1, text: "Retailer" },
-  { roleValue: 2, text: "Customer" },
-];
-
-export const defineRoutesByRole = [[], [], []];
