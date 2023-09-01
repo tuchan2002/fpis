24d2deb0121ca6ca3cda27ca2b80181e325d1a64
tuchan2002 <trananhtu171002@gmail.com>
2023-09-01 14:09:37 +0700

    push

--- api/hardhat.config.js
+++ api/hardhat.config.js
@@ -8,7 +8,7 @@ module.exports = {
     networks: {
         bscTestnet: {
             url: 'https://data-seed-prebsc-1-s1.binance.org:8545',
-            accounts: ["4a08a15e44f04306fc9a9dff0ae08dfdaf6951c9c10d41ec2b6d682247d07606"]
+            accounts: ['4a08a15e44f04306fc9a9dff0ae08dfdaf6951c9c10d41ec2b6d682247d07606']
         }
     },
     etherscan: {
