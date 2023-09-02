e5f576dfc0996fa802d3e82d0a07cbd2806b3697
tuchan2002 <trananhtu171002@gmail.com>
2023-09-02 14:04:07 +0700

    install mui lib

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,6 +1,10 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
