bceef7280006e9fc6578546c8db2991b2d389bce
tuchan2002 <trananhtu171002@gmail.com>
2023-09-09 14:23:23 +0700

    add middleware isAdmin

--- api/routes/authRoute.js
+++ api/routes/authRoute.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
 const authController = require('../controllers/authController');
 const isAuth = require('../middlewares/auth');
+const isAdmin = require('../middlewares/isAdmin');
 
 router.get('/get-auth', isAuth, authController.getAuth);
-router.post('/register', authController.register);
+router.post('/register', isAuth, isAdmin, authController.register);
 router.post('/login', authController.login);
 
 module.exports = router;
