2967e050ee016a81eeafd297ada1ece47ee52c3c
tuchan2002 <trananhtu171002@gmail.com>
2023-09-13 21:58:44 +0700

    test cicd

--- api/api-source/routes/authRoute.js
+++ api/api-source/routes/authRoute.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const authController = require('../controllers/authController');
 const isAuth = require('../middlewares/auth');
-const isAdmin = require('../middlewares/isAdmin');
+// const isAdmin = require('../middlewares/isAdmin');
 
 router.get('/get-auth', isAuth, authController.getAuth);
 router.post('/register', isAuth, authController.register);
