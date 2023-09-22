7b7ed51ce073d9d424908220a326475bc150cd48
tuchan2002 <trananhtu171002@gmail.com>
2023-09-22 10:47:43 +0700

    testcicd

--- fpis-contract/scripts/deploy.js
+++ fpis-contract/scripts/deploy.js
@@ -2,12 +2,10 @@ const fs = require('fs');
 const path = require('path');
 
 async function main() {
-    // eslint-disable-next-line no-undef
     const [deployer] = await ethers.getSigners();
 
     console.log('Deploying contracts with the account:', deployer.address);
 
-    // eslint-disable-next-line no-undef
     const contracts = await ethers.deployContract('FPIS');
 
     const contractAddress = await contracts.getAddress();
@@ -15,7 +13,7 @@ async function main() {
 
     const contractABI = JSON.parse(fs.readFileSync(path.join(__dirname, '..', 'artifacts/contracts/fpis.sol/FPIS.json'), 'utf8')).abi;
 
-    fs.writeFileSync(path.join(__dirname, '..', 'contract-info.json'), JSON.stringify({contractAddress, contractABI}));
+    fs.writeFileSync(path.join(__dirname, '../../react-app/src', 'contract-info.json'), JSON.stringify({contractAddress, contractABI}));
 }
 
 main()
