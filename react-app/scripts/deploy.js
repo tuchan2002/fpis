102fa6b37b8395f1fb291d535b44fdf528ce4900
tuchan2002 <trananhtu171002@gmail.com>
2023-09-22 11:45:09 +0700

    testcicd

--- react-app/scripts/deploy.js
+++ react-app/scripts/deploy.js
@@ -2,10 +2,12 @@ const fs = require('fs');
 const path = require('path');
 
 async function main() {
+    // eslint-disable-next-line no-undef
     const [deployer] = await ethers.getSigners();
 
     console.log('Deploying contracts with the account:', deployer.address);
 
+    // eslint-disable-next-line no-undef
     const contracts = await ethers.deployContract('FPIS');
 
     const contractAddress = await contracts.getAddress();
